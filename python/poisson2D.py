530647e43d0dfb6b005a128528f7971a67040240
aitzkora <marc.fuentes@inria.fr>
2013-03-25 13:50:30 +0100

    python :ajout des valeurs propes

--- python/poisson2D.py
+++ python/poisson2D.py
@@ -1,44 +1,58 @@
-import numpy as np
+from numpy import diag, ones , eye, kron, arange, pi, sin, cos, dot
+from numpy.linalg import norm
 def K1D(n):
     """
     Compute the 1D laplacian matrix
     
-    >>> np.diag(K1D(4)).tolist()
+    >>> diag(K1D(4)).tolist()
     [2.0, 2.0, 2.0, 2.0]
-    >>> np.diag(K1D(3),-1).tolist()
+    >>> diag(K1D(3),-1).tolist()
     [-1.0, -1.0]
-    >>> np.diag(K1D(4),1).tolist()
+    >>> diag(K1D(4),1).tolist()
     [-1.0, -1.0, -1.0]
+    >>> norm(val1D(3)*v1D(3)-dot(K1D(3),v1D(3)))<1e-10
+    True
     """
 
-    K =2*np.eye(n) - np.diag(np.ones(n-1),-1)\
-                   - np.diag(np.ones(n-1), 1)
+    K = 2 * eye(n) - diag(ones(n-1), -1)\
+                   - diag(ones(n-1),  1)
     return K
 
 
-def K2D(nx,ny):
-    return np.kron(K1D(nx),np.eye(ny)) + np.kron(eye(nx), np.eye(ny))
+def val1D(n, k = 1):
+     return (2 - 2 * cos(k * pi / (n + 1)))
 
-def compute_locale(self):
-        """
-        schema explicite 
+def v1D(n, k = 1):
+     return sin(arange(1, n + 1) * k * pi / (n + 1))
+
+def K2D(n):
+    """ computes the 2D laplacian on the [0,1]^2 square
+
+    >>v11=outer(v1D(3),v1D(3)).flatten(); norm(dot(K2D(3),v11)-2*val1D(3)*(2-1)**2*v11)<1e-10
+    True
     
-        u[i,j]^{n+1}-u[i,j]^{n} = dt/(h_x*h_y) * (u[i-1,j] + u[  
-        
-        """
-        diag_x = - 2.0 + self.hx*self.hx/(2.*self.dt)
-        diag_y = - 2.0 + self.hy*self.hy/(2.*self.dt)
-        w_x =  self.dt /(self.hx * self.hx)
-        w_y =  self.dt /(self.hy * self.hy)
-        u=self.u
-        u_out=u.copy()
-        u_out[1:-1, 1:-1] = (u[0:-2, 1:-1] + 
-                             u[2:  , 1:-1] + 
-                             u[1:-1, 1:-1] * diag_x ) * w_x +\
-                            (u[1:-1, 0:-2] + 
-                             u[1:-1, 2:  ] + 
-                             u[1:-1, 1:-1] * diag_y ) * w_y
+    """
+    return  (n-1)**2 * (kron(K1D(n), eye(n)) + kron(eye(n), K1D(n)))
 
+#def compute_locale(self):
+#        """
+#        schema explicite 
+#    
+#        u[i,j]^{n+1}-u[i,j]^{n} = dt/(h_x*h_y) * (u[i-1,j] + u[  
+#        
+#        """
+#        diag_x = - 2.0 + self.hx*self.hx/(2.*self.dt)
+#        diag_y = - 2.0 + self.hy*self.hy/(2.*self.dt)
+#        w_x =  self.dt /(self.hx * self.hx)
+#        w_y =  self.dt /(self.hy * self.hy)
+#        u=self.u
+#        u_out[1:-1, 1:-1] = (u[0:-2, 1:-1] + 
+#                             u[2:  , 1:-1] + 
+#                             u[1:-1, 1:-1] * diag_x ) * w_x +\
+#                            (u[1:-1, 0:-2] + 
+#                             u[1:-1, 2:  ] + 
+#                             u[1:-1, 1:-1] * diag_y ) * w_y
+#
 
 
 
