d6121082531e2d896ef17a032bc7b70ab3d363a6
aitzkora <marc.fuentes@inria.fr>
2013-06-14 14:53:11 +0200

    starting to coding the main function in the smc D version

--- smc/smc.d
+++ smc/smc.d
@@ -1,5 +1,7 @@
 import std.stdio;
 import std.random;
+import std.range;
+import std.algorithm;
 
 T[] cumsum(T)(T[] x) {
     auto y = x.dup;
@@ -12,20 +14,36 @@ T[] cumsum(T)(T[] x) {
 unittest {
   int[] z = [1, 4, 3];
   assert(cumsum!(int)(z) == [1, 5, 8]);
-};
+}
 
-double[] resample(double [] w, double [] x) {
+double[] resample(in double []  w, double [] x) {
    assert(x.length == w.length);
    auto W = cumsum(w);
-   auto u = w.dup 
-   foreach(ref r ; u) {
-       r = random(0.0, 1.0);
-   }
-   int idx = new int[w.length];
+   auto u = map!(a=>uniform(0.0, 1.0))(w).array();
+   auto idx = new int[w.length];
    foreach(i, uu ; u ) {
       foreach(j, ww ; W) {
-         idx[i] += (uu > ww) ? 1 ; 0;
+         idx[i] += (uu > ww) ? 1 : 0;
       }
    }
-    
+   return map!(a=>x[a])(idx).array();
 }
+
+
+Tuple!(double[][], double[]) smc(alias init, alias  logl, alias evol,
+                                 alias resa)(int T, double[] y, int N) {
+
+  
+    auto x = new double[][](N, T);
+    auto log_w = new double[][](N, T);
+    auto w = new double[][](N, T);
+    auto ess = new double[][](N, T);
+    
+    x[][0] = init(N); 
+    log_w[][0] = logl(y[0], x[][0]);
+
+
+
+
+}                               
+ 
