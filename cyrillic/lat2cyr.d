11fb0adef237af16bcb5b11db87f877a5a05f0b7
marc fuentes <marc.fuentes@inria.fr>
2016-10-06 23:40:51 +0200

    simplify the code of lat2cyr

--- cyrillic/lat2cyr.d
+++ cyrillic/lat2cyr.d
@@ -9,19 +9,13 @@ unittest {
 }
 
 string lat2cyr(in string mess) {
-    auto dž_sub = mess.replace("dž","џ");
-    auto lj_sub = dž_sub.replace("lj", "љ");
-    auto nj_sub = lj_sub.replace("nj", "њ");
-    auto DŽ_sub = nj_sub.replace("DŽ", "Џ");
-    auto LJ_sub = DŽ_sub.replace("LJ", "Љ");
-    auto NJ_sub = LJ_sub.replace("NJ", "Њ");
-    auto cyr_uni = "абвгдђежзијклмнопрстћуфхцчш";
-    auto lat_uni = "abvgdđežzijklmnoprstćufhcčš";
-    auto cyr_mess = tr(NJ_sub, lat_uni, cyr_uni);
-    auto CYR_uni = "АБВГДЂЕЖЗИЈКЛМНОПРСTЋУФХЦЧШ";
-    auto LAT_uni = "ABVGDĐEŽZIJKLMNOPRSTĆUFHCČŠ";
-    auto CYR_mess = tr(cyr_mess, LAT_uni, CYR_uni);
-    return CYR_mess;
+     auto double_sub = mess.replace("dž","џ").replace("DŽ", "Џ")
+                           .replace("lj", "љ").replace("LJ", "Љ")
+                           .replace("nj", "њ").replace("NJ", "Њ");
+    auto cyr_uni = "абвгдђежзијклмнопрстћуфхцчшАБВГДЂЕЖЗИЈКЛМНОПРСTЋУФХЦЧШ";
+    auto lat_uni = "abvgdđežzijklmnoprstćufhcčšABVGDĐEŽZIJKLMNOPRSTĆUFHCČŠ";
+    auto cyr_mess = tr(double_sub, lat_uni, cyr_uni);
+    return cyr_mess;
 }
 
 void main() {
