82c56aa34395202d20907f617c029e47c3655443
aitzkora <marc.fuentes@inria.fr>
2025-01-15 22:30:14 +0100

    do some progress on the batman puzzle

--- ikasi/batman/client.d
+++ ikasi/batman/client.d
@@ -1,33 +1,55 @@
 #!/usr/bin/env rdmd
 import std;
 import std.socket;
+import std.stdio;
 
-int[2] find_bomb(int W, int H, int N, int x0, int y0, ref Socket soc)
-{
-  int[2] xy = [x0, y0];
-  int i = 0;
-  while(i < N) 
-  {
-     
-  i++;
-  }
-  if (i < N)  writeln("les otages sont vivants 👍!");
-  else writeln("les otages sont morts 👎!");
-  return xy;
+struct Config {
+   int W;
+   int H;
+   int n;
+   int[2] x;
 }
+ 
+
+
+string recv(ref Socket soc) {
+   char[64] buff;
+   auto z = soc.receive(buff);
+   return to!string(buff.toStringz).chomp;
+}
+
+int[2] saute(in string dir, ref Config c)
+{
+  
+ return [0,0];
+}
+
 
 void main() {
     auto soc = new Socket(AddressFamily.UNIX, SocketType.STREAM);
     soc.connect(new UnixAddress("/tmp/oracle"));
 
-    import std.stdio;
-    char[64] buff;
-    //writeln(buf[0 .. soc.receive(buf)]);
-    auto z = soc.receive(buff);
-    auto param1 = to!string(buff.toStringz).chomp.split(" ");
-    int W = param1[0].to!int;
-    int H = param1[1].to!int;
-    write("taille de l'immeuble", W, "x", H);
+    auto param1 = recv(soc).split(" ").map!(to!int);
+    int W = param1[0];
+    int H = param1[1];
+    writeln("taille de l'immeuble :", W, " x ", H);
+    int N = recv(soc).to!int;
+    writeln("nombre de tours : ", N);
+    auto batPos = recv(soc).split(" ").map!(to!int);
+    writeln("batman est en : ", batPos);
+    auto conf = Config(W,H, N, [batPos[0], batPos[1]]);
+    while (true) {
+      string dir = recv(soc);
+      if (dir == "F" || conf.n == 0) break;
+      saute(dir, conf);
+      char[64] buff;
+      sprintf(buff.ptr, "%d %d", conf.x[0], conf.x[1]);
+      soc.send(buff);
+    }
     soc.close();
+    if (conf.n > 0)  
+      writeln("les otages sont vivants 👍!");
+    else 
+      writeln("les otages sont morts 👎!");
 }
 
--- ikasi/batman/server.d
+++ ikasi/batman/server.d
@@ -1,6 +1,9 @@
 #!/usr/bin/env rdmd
 import std.socket;
 import core.stdc.stdio;
+import std.numeric;
+import std.math;
+import std.stdio;
 struct Problem {
   int W;
   int H;
@@ -10,7 +13,34 @@ struct Problem {
 }
 
 
+pure string calc_dir_dist(int[2] x0, int[2] xB)
+{
+  immutable(string[int[2]]) dirs = [ [0,-1] : "U", [1, -1] : "UR", [1 , 0] : "R", [1,1] : "DR",
+                                     [0, 1] : "D", [-1, 1] : "DL", [-1, 0] : "L", [-1, -1] : "UL"];
+  int[2] dx = xB[] - x0[];
+  int hyp2 = dotProduct(dx, dx);
+  int dxOne = sgn(dx[0]) * (4*dx[0]*dx[0]  > hyp2);
+  int dyOne = sgn(dx[1]) * (4*dx[0]*dx[0] <= 3 * hyp2);
+  return dirs[[dxOne, dyOne]];
+}
+
+pure string calc_dir_lexico(int[2] x0, int[2] xB)
+{
+  immutable(string[int[2]]) dirs = [ [0,-1] : "U", [1, -1] : "UR", [1 , 0] : "R", [1,1] : "DR",
+                                     [0, 1] : "D", [-1, 1] : "DL", [-1, 0] : "L", [-1, -1] : "UL",
+                                     [0,0]  : "F"];
+
+  int[2] dx = xB[] - x0[];
+  int dxOne = (dx[0] >= 0 ? 1 : -1);
+  int dyOne = (dx[1] >= 0 ? 1 : -1);
+  return dirs[[dxOne, dyOne]];
+}
+unittest {
+   assert(calc_dir_lexico([5,5], [7,4]) == "UR");
+}
+
 void main() {
+    writeln(calc_dir_lexico([5,5], [7,4]));
     auto listener = new Socket(AddressFamily.UNIX, SocketType.STREAM);
     listener.bind(new UnixAddress("/tmp/oracle"));
     listener.listen(2);
@@ -22,10 +52,16 @@ void main() {
     char[64] line;
     sprintf(line.ptr, "%d %d\n", building.W, building.H);
     soc.send(line);
+    sprintf(line.ptr, "%d\n", building.N);
+    soc.send(line);
+    sprintf(line.ptr, "%d %d\n", building.x0[0], building.x0[1]);
+    soc.send(line);
 
-    //while(1) {
-    //    soc.send("trucmuche\n");
-    //}
+    int i = 0;
+    while(i<building.N) {
+        soc.send("");
+        i++;
+    }
     soc.close();
 
     listener.close();
